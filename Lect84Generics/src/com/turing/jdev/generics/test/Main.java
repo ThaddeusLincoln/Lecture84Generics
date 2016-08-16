1586683c1b74d90a6bf6801a45cafc16977b38fe
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-16 19:48:59 +0200

    As it can be seen in the main method, using raw types, could cause errors on rutime, because the use of raw types is preventing the compiler from doing the type checked.

--- Lect84Generics/src/com/turing/jdev/generics/test/Main.java
+++ Lect84Generics/src/com/turing/jdev/generics/test/Main.java
@@ -1,10 +1,26 @@
 package com.turing.jdev.generics.test;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args){
 		
+		ArrayList items = new ArrayList();
+		items.add(1);
+		items.add(2);
+		items.add(3);
+		items.add("UN TEXTO CUALQUIERA");
+		items.add(4);
+		items.add(5);
 		
+		printDoubled(items);
 		
+	}
+	
+	private static void printDoubled(ArrayList list){
+		for(Object obj : list){
+			System.out.println((Integer) obj * 2);
+		}
 	}
 }
