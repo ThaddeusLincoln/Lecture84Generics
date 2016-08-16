0d6792aeb5ed92d0c295a6ea25122e3a4297811d
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-16 19:54:56 +0200

    By using parametized types: ArrayList<Integer> items = new ArrayList<Integer>(); we improve the behavior, we won't have to cast, and we'll be only able to pass objects of a given type. But we still can improve.

--- Lect84Generics/src/com/turing/jdev/generics/test/Main.java
+++ Lect84Generics/src/com/turing/jdev/generics/test/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
 	public static void main(String[] args){
 		
-		ArrayList items = new ArrayList();
+		ArrayList<Integer> items = new ArrayList<Integer>();
 		items.add(1);
 		items.add(2);
 		items.add(3);
-		items.add("UN TEXTO CUALQUIERA");
+		// items.add("UN TEXTO CUALQUIERA");
 		items.add(4);
 		items.add(5);
 		
@@ -18,9 +18,9 @@ public class Main {
 		
 	}
 	
-	private static void printDoubled(ArrayList list){
-		for(Object obj : list){
-			System.out.println((Integer) obj * 2);
+	private static void printDoubled(ArrayList<Integer> list){
+		for(int num : list){
+			System.out.println(num * 2);
 		}
 	}
 }
